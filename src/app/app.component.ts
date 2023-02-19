8df18a8a6c041e54b67e22f0723fc6094fc9e5fc
nsay <narithsay@gmail.com>
2023-02-19 12:01:39 -0500

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.module';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  
+    cityName: string;
+    weatherData?: WeatherData;
+    
+    constructor(private weatherService: WeatherService) {
+        this.cityName = 'Wellington';
+    }
+
+    ngOnInit(): void {
+        this.getWeatherData(this.cityName);
+        this.cityName = '';
+    }
+
+    onSubmit() {
+        this.getWeatherData(this.cityName);
+        this.cityName = '';
+    }
+
+    private getWeatherData(cityName: string) {
+        this.weatherService.getWeatherData(cityName).subscribe({
+            next: (response) => {
+                this.weatherData = response;
+                console.log(response);
+            }
+        });
+    }
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { WeatherData } from '../models/weather.module';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class WeatherService {
+
+	constructor(private http: HttpClient) { }
+
+	getWeatherData(cityName:string):Observable<WeatherData>{
+		return this.http.get<WeatherData>(environment.weatherApiBaseUrl+'/city/'+cityName, {
+		  	headers: new HttpHeaders()
+		  	.set(environment.XRapidAPIHostName, environment.XRapidAPIHostValue)
+		  	.set(environment.XRapidAPIKeyName,environment.XRapidAPIKeyValue)
+		});
+	}
+}
